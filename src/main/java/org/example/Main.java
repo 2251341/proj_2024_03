644a14f6db55801174a5d51216b2f8ec90c533a9
2251341 <dydwksos246@naver.com>
2024-03-20 13:53:34 +0900

    프로젝트

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("안녕");
+        System.out.println("== 프로젝트 시작 ==");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("명령어) ");
+        String cmd = sc.next();
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+
+
+        System.out.printf("명령어) ");
+        int num = sc.nextInt();
+        sc.nextLine();
+        System.out.printf("입력된 명령어 : %s\n", num);
+
+        sc.close();
+
+        System.out.println("== 프로젝트 끝 ==");
+
     }
 }
